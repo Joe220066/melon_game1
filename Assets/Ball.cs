05d7bb49f1def437c0984952d2c89645ac62a1d1
su2u4-1 <Joey23.su@gmail.com>
2024-01-12 13:50:48 +0800

    Revert "Merge branch 'main' of https://github.com/Joe220066/melon_game1"
    This reverts commit 28f3793e7f4dd8ceb7a6e099d5f976f526715978.

---
+++ Assets/Ball.cs
@@ -0,0 +1,124 @@
+using System;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    float size = 0;
+    [SerializeField] GameObject ball;
+    float[] size_array = { 0.4f, 0.5f, 0.62f, 0.77f, 0.95f, 1.18f, 1.47f, 1.83f, 2.27f, 2.82f, 3.5f };
+    GameObject pl;
+    GameObject text;
+    private static bool ce = true;
+    int nextball;
+    [SerializeField] Sprite i0;
+    [SerializeField] Sprite i1;
+    [SerializeField] Sprite i2;
+    [SerializeField] Sprite i3;
+    [SerializeField] Sprite i4;
+    [SerializeField] Sprite i5;
+    [SerializeField] Sprite i6;
+    [SerializeField] Sprite i7;
+    [SerializeField] Sprite i8;
+    [SerializeField] Sprite i9;
+    [SerializeField] Sprite i10;
+    void Resize(int s)
+    {
+        if (s == 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = i0;
+        }
+        else if (s == 1)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = i1;
+        }
+        else if (s == 2)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = i2;
+        }
+        else if (s == 3)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = i3;
+        }
+        else if (s == 4)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = i4;
+        }
+        else if (s == 5)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = i5;
+        }
+        else if (s == 6)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = i6;
+        }
+        else if (s == 7)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = i7;
+        }
+        else if (s == 8)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = i8;
+        }
+        else if (s == 9)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = i9;
+        }
+        else if (s == 10)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = i10;
+        }
+        size = size_array[s];
+        transform.tag = $"ball{s}";
+        transform.localScale = new UnityEngine.Vector3(size, size, 0f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ce = true;
+        if (size == 0)
+        {
+            System.Random ri = new System.Random(Guid.NewGuid().GetHashCode());
+            Resize(nextball);
+            nextball = ri.Next(0, 3);
+        }
+        pl = GameObject.Find("Player");
+        text = GameObject.Find("Text");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<Transform>().position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == transform.tag)
+        {
+            if (ce)
+            {
+                UnityEngine.Vector3 point = coll.gameObject.transform.position;
+                UnityEngine.Vector3 pos = GetComponent<Transform>().position;
+                float x = (point.x + pos.x) / 2;
+                float y = (point.y + pos.y) / 2;
+                int s = Int32.Parse(transform.tag[4..]);
+                Destroy(coll.gameObject);
+                text.SendMessage("Rescore", s + 1);
+                if (s < 11)
+                {
+                    string b = $"{x}/{y}/{s + 1}";
+                    pl.SendMessage("NewBall", b);
+                }
+                ce = false;
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            ce = true;
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] GameObject Instantiate_Position;
+    [SerializeField] GameObject ball;
+    [SerializeField] float MoveSpeed;
+    bool i = true;
+    int n = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 pos = GetComponent<Transform>().position;
+        transform.Translate(-4.5f-pos[0], 4f-pos[1], -pos[2]);
+        n = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = GetComponent<Transform>().position;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (pos[0] > -7)
+            {
+                transform.Translate(-MoveSpeed*Time.deltaTime,0f,0f);
+            }
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (pos[0] < -2)
+            {
+                transform.Translate(MoveSpeed * Time.deltaTime, 0f, 0f);
+            }
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) && i)
+        {
+            Vector3 pl = Instantiate_Position.transform.position;
+            System.Random ri = new System.Random(Guid.NewGuid().GetHashCode());
+            pl.x += (float)ri.NextDouble() * 0.02f - 0.01f;
+            GameObject t = Instantiate(ball, pl, Quaternion.identity);
+            t.name = $"ball{n}";
+            n++;
+            i = false;
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            i = true;
+        }
+    }
+    void NewBall(string s)
+    {
+        string[] str = s.Split("/");
+        Vector3 pos;
+        pos.x = float.Parse(str[0]);
+        pos.y = float.Parse(str[1]);
+        pos.z = 0f;
+        GameObject t = Instantiate(ball, pos, Quaternion.identity);
+        t.name = $"ball{n}";
+        n++;
+        t.SendMessage("Resize",int.Parse(str[2]));
+    }
+}
