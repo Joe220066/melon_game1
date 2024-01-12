ecd61bde7e7c91292df1c756f8bafb0e12a64fb9
su2u4-1 <Joey23.su@gmail.com>
2024-01-12 13:49:08 +0800

    20240112

---
+++ Assets/C#_script/score.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    int score;
+    GameObject te;
+    void Rescore(int s)
+    {
+        score += s;
+
+
+    }
+    void Start()
+    {
+        te = GameObject.Find("Text");
+        te.GetComponent<TextMeshProUGUI>().text = "score:";
+    }
+    void Update()
+    {
+        te.GetComponent<TextMeshProUGUI>().text = $"score:{score}";
+    }
+}
