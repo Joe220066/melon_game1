2b18104eb815fba5abbab424c8a015d942993d40
su2u4 <Joey23.su@gmail.com>
2024-02-23 14:36:19 +0800

    202402230

---
+++ Assets/nextBall.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class nextBall : MonoBehaviour
+{
+    [SerializeField] GameObject ball;
+    readonly float[] size_array = { 0.4f, 0.5f, 0.62f, 0.77f, 0.95f, 1.18f, 1.47f, 1.83f, 2.27f, 2.82f, 3.5f };
+    GameObject pl;
+    GameObject text;
+    private static bool ce = true;
+    [SerializeField] Sprite[] img;
+    void Resize(int s)
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = img[s];
+        float size = size_array[s];
+        transform.tag = $"ball{s}";
+        transform.localScale = new Vector3(size, size, 0f);
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Resize(Player.nextball);
+    }
+}
